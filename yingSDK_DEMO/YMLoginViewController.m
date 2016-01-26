5fc582ac83f35e5ae55dd279f5a30776b2fbb109
yangaqi <yangaqi@yangaqideMBP-2.lan>
2016-01-26 18:14:15 +0800

    获取盈米token

--- yingSDK_DEMO/YMLoginViewController.m
+++ yingSDK_DEMO/YMLoginViewController.m
@@ -14,12 +14,15 @@
 {
     Deferred *d;
     NSString *token;
+    NSString *access_token;
 }
 
 @property (nonatomic) BOOL isInit;
 
 @property (weak, nonatomic) IBOutlet UITextField *txt_accountId;
 @property (weak, nonatomic) IBOutlet UITextField *txt_password;
+@property (weak, nonatomic) IBOutlet UIButton *btnToken;
+@property (weak, nonatomic) IBOutlet UILabel *lb_token;
 @end
 
 @implementation YMLoginViewController
@@ -51,13 +54,34 @@
     
     //1.第三方用户登录，获取第三方用户token 注：这地方是个模拟过程
     d = [HttpClient doPost:@"http://172.19.0.244/ymb/v1/user/login" parameters:@{@"name":name,@"password":password}];
-
+    __weak YMLoginViewController *weakSelf = self;
     [d then:^id(id resultObject) {
         NSDictionary *dict = (NSDictionary*)resultObject;
         if(dict){
             //2.
             token = dict[@"token"];
+            weakSelf.btnToken.enabled = YES;
+            
         }
+        return resultObject;
+    } failure:^id(id resultObject) {
+        return resultObject;
+    }];
+}
+
+
+- (IBAction)token:(id)sender {
+    //3.获取盈米token 注：这地方是个模拟过程
+    d = [HttpClient doGet:@"http://172.19.0.244/ymb/v1/user/token" parameters:@{@"access_token":token}];
+    
+    [d then:^id(id resultObject) {
+        NSDictionary *dict = (NSDictionary*)resultObject;
+        if(dict){
+            //4.
+            access_token = dict[@"accessToken"];
+            self.lb_token.text = access_token;
+        }
+        
         return resultObject;
     } failure:^id(id resultObject) {
         return resultObject;
